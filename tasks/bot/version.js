97ece9e952082f0930ac42ec71c95f6ff32037b3
EOussama <oussama_gtacj@live.fr>
2019-07-30 10:47:46 +0100

    Better versionning task

--- tasks/bot/version.js
+++ tasks/bot/version.js
@@ -5,6 +5,7 @@
 var path = require('path');
 var gulp = require('gulp');
 var bump = require('gulp-bump');
+var replace = require('gulp-replace');
 
 /**
  * Bumps the version
@@ -13,13 +14,21 @@ var bump = require('gulp-bump');
  */
 module.exports.sem = function (mode) {
 
-  // Resolving the path
+  // Resolving the paths
   var packageDir = path.resolve(__dirname, './../../src/bot/');
   var packageFiles = path.resolve(packageDir, 'package*.json');
+  var envFile = path.resolve(packageDir, '.env');
 
-  return gulp.src(packageFiles)
+  // Getting the current version
+  var currVersion = require(path.resolve(packageDir, 'package.json')).version;
+
+  // Proccessing the task
+  return gulp.src(envFile)
+    .pipe(replace(/META_VERSION=.*/g, 'META_VERSION=' + currVersion))
+    .pipe(gulp.dest(packageDir))
+    .pipe(gulp.src(packageFiles))
     .pipe(bump({ type: mode }))
-    .pipe(gulp.dest(path.resolve(packageDir)));
+    .pipe(gulp.dest(packageDir));
 }
 
 /**
@@ -29,11 +38,16 @@ module.exports.sem = function (mode) {
  */
 module.exports.specific = function (version) {
 
-  // Resolving the path
+  // Resolving the paths
   var packageDir = path.resolve(__dirname, './../../src/bot/');
   var packageFiles = path.resolve(packageDir, 'package*.json');
+  var envFile = path.resolve(packageDir, '.env');
 
-  return gulp.src(packageFiles)
-    .pipe(bump({ version }))
+  // Proccessing the task
+  return gulp.src(envFile)
+    .pipe(replace(/META_VERSION=.*/g, 'META_VERSION=' + version))
+    .pipe(gulp.dest(packageDir))
+    .pipe(gulp.src(packageFiles))
+    .pipe(bump({ version: version }))
     .pipe(gulp.dest(packageDir));
 }
